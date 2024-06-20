53e825d4d6aca94f2603c303297406611a6709a1
Joao Vitor Barbosa <jv.blima11@gmail.com>
2024-06-20 19:40:50 -0400

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/front_end_mentor_newsletter-sign"
+  base: "/front_end_mentor_newsletter-sign",
 })
